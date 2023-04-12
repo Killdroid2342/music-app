be59ec7a90b150b70f98c137d9556baa3d5f4c41
Haroon Khan <HaroonKhan120704@gmail.com>
2023-04-12 01:04:29 +0100

    Cleaning app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
 import React, { useState, useRef } from 'react';
+import SavedSongs from './components/SavedSongs';
+import DisplayedSongs from './components/DisplayedSongs';
+import Controls from './components/Controls';
+import ImportingFiles from './components/ImportingFiles';
 
 interface Song {
   name: string;
@@ -72,87 +76,23 @@ export default function App(): JSX.Element {
     <>
       <h1 className='text-center text-3xl font-bold font-mono'>Music Player</h1>
       <div className='flex flex-row h-screen'>
-        <div className='border border-white'>
-          <p>SONGS HERE</p>
-          <div className='border border-white'></div>
-          <div className='flex flex-col'>
-            {songs.map((song, index) => (
-              <p
-                key={index}
-                onClick={() => handleSongClick(song)}
-                className='cursor-pointer border border-red-900 text-black bg-white font-bold'
-              >
-                {song.name}
-              </p>
-            ))}
-          </div>
-          <div className='w-full border border-white'></div>
-        </div>
+        <SavedSongs handleSongClick={handleSongClick} songs={songs} />
         <div className='border border-red-900 w-10/12 flex flex-col justify-center items-center'>
           <p className='mt-4'>{currentSong?.name ?? 'Song Name'}</p>
-          <div className='border-dashed border border-purple-900 h-3/5 w-3/5 mt-4'>
-            {currentSong?.dataUrl ? (
-              <audio
-                ref={audioRef}
-                controls={false}
-                onEnded={() => setCurrentSong(null)}
-                onError={() => setCurrentSong(null)}
-              >
-                <source src={currentSong.dataUrl} type='audio/mpeg' />
-              </audio>
-            ) : (
-              <p>IMG</p>
-            )}
-          </div>
-          <div className='border border-white w-full flex flex-row justify-evenly'>
-            <div className='border border-red-900 p-3 cursor-pointer flex flex-col'>
-              <input
-                type='range'
-                min='0'
-                max='1'
-                step='0.01'
-                onChange={handleVolumeChange}
-              />
-              Music Volume
-            </div>
-
-            <div className='flex flex-row border border-blue-400'>
-              <p className='border border-blue-900 p-3 cursor-pointer'>
-                {'<<'}
-              </p>
-              <p
-                className='border border-blue-900 p-3 cursor-pointer'
-                onClick={handlePlayPauseClick}
-              >
-                {'No Songs' ? <>{isPlaying ? '▐▐' : ' ▶ '}</> : 'Song Name'}
-              </p>
-              <p className='border border-blue-900 p-3 cursor-pointer'>
-                {'>>'}
-              </p>
-            </div>
-            <div className='flex flex-row border border-green-400'>
-              <p
-                className='border border-green-900 p-3 cursor-pointer'
-                onClick={handleSkipBackwardClick}
-              >
-                Back 10
-              </p>
-              <p
-                className='border border-green-900 p-3 cursor-pointer'
-                onClick={handleSkipForwardClick}
-              >
-                Forward 10
-              </p>
-            </div>
-          </div>
-          <div className='border border-purple-900 w-full text-center mt-20'>
-            Music bar
-          </div>
+          <DisplayedSongs
+            setCurrentSong={setCurrentSong}
+            audioRef={audioRef}
+            currentSong={currentSong}
+          />
+          <Controls
+            handleVolumeChange={handleVolumeChange}
+            handlePlayPauseClick={handlePlayPauseClick}
+            isPlaying={isPlaying}
+            handleSkipBackwardClick={handleSkipBackwardClick}
+            handleSkipForwardClick={handleSkipForwardClick}
+          />
         </div>
-        <div className='border border-blue-900 w-56 flex flex-col'>
-          <p>PUTTING SONGS HERE</p>
-          <input type='file' onChange={handleFileUpload} />
-        </div>
+        <ImportingFiles />
       </div>
     </>
   );
