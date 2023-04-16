277223eb981f5ef8ec43d0c592b3edfa950f719e
Haroon Khan <HaroonKhan120704@gmail.com>
2023-04-16 01:11:11 +0100

    putting original app in pages file

---
+++ src/Pages/Pages.tsx
@@ -0,0 +1,74 @@
+import React, { useState, useRef } from 'react';
+import SavedSongs from '../components/SavedSongs';
+import Controls from '../components/Controls';
+import ImportingFiles from '../components/ImportingFiles';
+import ProgressBar from '../components/ProgressBar';
+
+export interface Song {
+  name: string;
+  dataUrl: string;
+}
+
+export default function Pages(): JSX.Element {
+  const [songs, setSongs] = useState<Song[]>([]);
+  const [currentSong, setCurrentSong] = useState<Song | null>(null);
+  const audioRef = useRef<HTMLAudioElement>(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [progress, setProgress] = useState<number>(0);
+
+  const handleSongClick = (song: Song) => {
+    setCurrentSong(song);
+    if (audioRef.current) {
+      audioRef.current.load();
+      audioRef.current.play();
+    }
+  };
+
+  return (
+    <>
+      <h1 className='text-center text-3xl font-bold font-mono'>Music Player</h1>
+      <div className='flex flex-row justify-end mr-5'>
+        <p>Account: ADMIN</p>
+        <button className='border border-white'>Sign Out</button>
+      </div>
+      <div className='flex flex-row h-screen'>
+        <SavedSongs handleSongClick={handleSongClick} songs={songs} />
+        <div className='border border-red-900 w-10/12 flex flex-col justify-center items-center'>
+          <p className='mt-4 text-2xl'>{currentSong?.name ?? 'Song Name'}</p>
+          {currentSong?.dataUrl ? (
+            <audio
+              ref={audioRef}
+              controls={false}
+              onEnded={() => setCurrentSong(null)}
+              onError={() => setCurrentSong(null)}
+              onTimeUpdate={() => {
+                if (audioRef.current) {
+                  const progress =
+                    (audioRef.current.currentTime / audioRef.current.duration) *
+                    100;
+                  setProgress(progress);
+                }
+              }}
+            >
+              <source src={currentSong.dataUrl} type='audio/mpeg' />
+            </audio>
+          ) : null}
+          <Controls
+            audioRef={audioRef}
+            isPlaying={isPlaying}
+            setIsPlaying={setIsPlaying}
+            currentSong={currentSong}
+            setCurrentSong={setCurrentSong}
+            songs={songs}
+          />
+          <ProgressBar
+            progress={progress}
+            currentSong={currentSong}
+            audioRef={audioRef}
+          />
+        </div>
+        <ImportingFiles songs={songs} setSongs={setSongs} />
+      </div>
+    </>
+  );
+}
