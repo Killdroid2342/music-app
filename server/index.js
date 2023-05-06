2e391d58e7217394ad89915b77bfbc19870e28c5
Haroon Khan <HaroonKhan120704@gmail.com>
2023-05-06 14:23:10 +0100

    implementing cors

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 const routes = require('./api/index');
+
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+app.use(cors(corsOptions));
 
 routes(app, { urlencodedParser });
 
