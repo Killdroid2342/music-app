269aacf35f4771fb05616367a1684eabfda392e4
Haroon Khan <HaroonKhan120704@gmail.com>
2023-05-08 00:01:35 +0100

    re done login and register

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -2,27 +2,45 @@ import React from 'react';
 
 const Login = ({ handleLoginSubmit, handleLoginInputChange }: any) => {
   return (
-    <div className='login'>
-      <form className='form' onSubmit={handleLoginSubmit}>
-        <label htmlFor='chk' aria-hidden='true'>
-          Log in
-        </label>
-        <input
-          className='input'
-          type='text'
-          name='username'
-          placeholder='Username'
-          onChange={handleLoginInputChange}
-        />
-        <input
-          className='input'
-          type='password'
-          name='password'
-          placeholder='Password'
-          onChange={handleLoginInputChange}
-        />
-        <button type='submit'>Log in</button>
-      </form>
+    <div className='flex justify-center h-screen bg-screen'>
+      <div className='my-auto p-4 w-96 rounded-lg bg-black bg-opacity-20 border border-white'>
+        <h1 className='text-2xl my-8 text-hawkes-blue-500 text-center'>
+          Login
+        </h1>
+        <form className='py-2' onSubmit={handleLoginSubmit}>
+          <div className='flex flex-col mx-auto w-fit'>
+            <label htmlFor='' aria-hidden='true'>
+              Username
+            </label>
+            <input
+              type='text'
+              name='username'
+              placeholder='Username'
+              onChange={handleLoginInputChange}
+              className='text-black'
+            />
+          </div>
+          <div className='flex flex-col mx-auto w-fit'>
+            <label htmlFor='' className='text-sm text-hawkes-blue-500'>
+              Password
+            </label>
+            <input
+              type='password'
+              name='password'
+              placeholder='Password'
+              onChange={handleLoginInputChange}
+              className='text-black'
+            />
+          </div>
+          <div className='flex flex-col mx-auto w-fit'>
+            <input
+              type='submit'
+              value='Login'
+              className='btn mt-6 cursor-pointer'
+            />
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -2,27 +2,45 @@ import React from 'react';
 
 const Register = ({ handleRegisterSubmit, handleRegisterInputChange }: any) => {
   return (
-    <div className='register'>
-      <form className='form' onSubmit={handleRegisterSubmit}>
-        <label htmlFor='chk' aria-hidden='true'>
+    <div className='flex justify-center h-screen bg-screen'>
+      <div className='my-auto p-4 w-96 rounded-lg bg-black bg-opacity-20 border border-white'>
+        <h1 className='text-2xl my-8 text-hawkes-blue-500 text-center'>
           Register
-        </label>
-        <input
-          className='input'
-          type='text'
-          name='username'
-          placeholder='Username'
-          onChange={handleRegisterInputChange}
-        />
-        <input
-          className='input'
-          type='password'
-          name='password'
-          placeholder='Password'
-          onChange={handleRegisterInputChange}
-        />
-        <input type='submit' value='Register' className='regButton' />
-      </form>
+        </h1>
+        <form className='py-2' onSubmit={handleRegisterSubmit}>
+          <div className='flex flex-col mx-auto w-fit'>
+            <label htmlFor='' aria-hidden='true'>
+              Username
+            </label>
+            <input
+              type='text'
+              name='username'
+              placeholder='Username'
+              onChange={handleRegisterInputChange}
+              className='text-black'
+            />
+          </div>
+          <div className='flex flex-col mx-auto w-fit'>
+            <label htmlFor='' className='text-sm text-hawkes-blue-500'>
+              Password
+            </label>
+            <input
+              type='password'
+              name='password'
+              placeholder='Password'
+              onChange={handleRegisterInputChange}
+              className='text-black'
+            />
+          </div>
+          <div className='flex flex-col mx-auto w-fit'>
+            <input
+              type='submit'
+              value='Register'
+              className='btn mt-6 cursor-pointer'
+            />
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
