a6bf20ffb56179555f757de51cb269b541ae6669
Haroon Khan <HaroonKhan120704@gmail.com>
2023-05-10 00:06:15 +0100

    re done register

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,45 +1,50 @@
 import React from 'react';
 
-const Register = ({ handleRegisterSubmit, handleRegisterInputChange }: any) => {
+const Register = ({
+  handleRegisterSubmit,
+  handleRegisterInputChange,
+  changeForm,
+  form,
+}: any) => {
   return (
     <div className='flex justify-center h-screen bg-screen'>
       <div className='my-auto p-4 w-96 rounded-lg bg-black bg-opacity-20 border border-white'>
-        <h1 className='text-2xl my-8 text-hawkes-blue-500 text-center'>
+        <h1 className='text-2xl my-8 text-hawkes-blue-500 text-center font-bold'>
           Register
         </h1>
         <form className='py-2' onSubmit={handleRegisterSubmit}>
           <div className='flex flex-col mx-auto w-fit'>
-            <label htmlFor='' aria-hidden='true'>
-              Username
-            </label>
+            <label className='text-lg py-2'>Username</label>
             <input
               type='text'
               name='username'
               placeholder='Username'
               onChange={handleRegisterInputChange}
-              className='text-black'
+              className='p-2 rounded-md text-black'
             />
           </div>
           <div className='flex flex-col mx-auto w-fit'>
-            <label htmlFor='' className='text-sm text-hawkes-blue-500'>
-              Password
-            </label>
+            <label className='text-lg py-2 pt-5'>Password</label>
             <input
               type='password'
               name='password'
               placeholder='Password'
               onChange={handleRegisterInputChange}
-              className='text-black'
+              className='p-2 rounded-md text-black'
             />
           </div>
           <div className='flex flex-col mx-auto w-fit'>
             <input
               type='submit'
               value='Register'
-              className='btn mt-6 cursor-pointer'
+              className='mt-10 text-xl cursor-pointer border border-white p-3 rounded-lg'
             />
           </div>
         </form>
+        <p
+          onClick={changeForm}
+          className='text-center mt-10 font-bold cursor-pointer'
+        >{`Go To ${form}`}</p>
       </div>
     </div>
   );
