997e08c83537455f4c5f2a15c8133cc463ae0e99
Haroon Khan <HaroonKhan120704@gmail.com>
2023-05-23 00:33:42 +0100

    fixing auth

--- server/api/auth.js
+++ server/api/auth.js
@@ -2,28 +2,30 @@ const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 const { verifyToken } = require('../modal/token');
+const bodyParser = require('body-parser');
 require('dotenv').config();
+router.use(bodyParser.json());
 
 router.post('/validate-token', async (req, res) => {
   console.log('This is validate token');
   const { token } = req.body;
+  console.log(token);
+  // try {
+  //   console.log(token);
 
-  try {
-    console.log(token);
+  //   const decodedToken = jwt.verify(token, process.env.acsessToken);
+  //   const { username } = decodedToken;
 
-    const decodedToken = jwt.verify(token, process.env.acsessToken);
-    const { username } = decodedToken;
-
-    const { clientUsername } = await verifyToken(username);
-    if (clientUsername === username) {
-      res.send('Good Token');
-    } else {
-      res.send('Bad Token');
-    }
-  } catch (error) {
-    console.log(error);
-    res.status(500).send('Error validating token');
-  }
+  //   const { clientUsername } = await verifyToken(username);
+  //   if (clientUsername === username) {
+  //     res.send('Good Token');
+  //   } else {
+  //     res.send('Bad Token');
+  //   }
+  // } catch (error) {
+  //   console.log(error);
+  //   res.status(500).send('Error validating token');
+  // }
 });
 
 module.exports = router;
--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 4000;
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
