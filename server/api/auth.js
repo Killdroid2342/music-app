1c25b7939b00d6d0c0d7d6cb24e27b176b40ab22
Haroon Khan <HaroonKhan120704@gmail.com>
2023-05-24 23:09:29 +0100

    re doing logic

--- server/api/auth.js
+++ server/api/auth.js
@@ -10,22 +10,11 @@ router.post('/validate-token', async (req, res) => {
   console.log('This is validate token');
   const { token } = req.body;
   console.log(token);
-  // try {
-  //   console.log(token);
+  try {
+    console.log(token);
 
-  //   const decodedToken = jwt.verify(token, process.env.acsessToken);
-  //   const { username } = decodedToken;
-
-  //   const { clientUsername } = await verifyToken(username);
-  //   if (clientUsername === username) {
-  //     res.send('Good Token');
-  //   } else {
-  //     res.send('Bad Token');
-  //   }
-  // } catch (error) {
-  //   console.log(error);
-  //   res.status(500).send('Error validating token');
-  // }
+    const decodedToken = jwt.verify(token, process.env.acsessToken);
+  } catch (error) {}
 });
 
 module.exports = router;
