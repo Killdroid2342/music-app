223cb02925d349bd45cc76eb3106cb17bf6dd22e
Haroon Khan <HaroonKhan120704@gmail.com>
2023-05-26 18:48:26 +0100

    fixing auth js

--- server/api/auth.js
+++ server/api/auth.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const jwt = require('jsonwebtoken');
 const { verifyToken } = require('../modal/token');
 const bodyParser = require('body-parser');
 require('dotenv').config();
@@ -10,8 +9,16 @@ router.post('/validate-token', async (req, res) => {
   console.log('This is validate token');
   const { token } = req.body;
   console.log(token);
-  console.log(token);
   console.log(await verifyToken(token));
+  if ((await verifyToken(token)) !== false) {
+    res.send({
+      message: 'correct token',
+    });
+  } else {
+    res.send({
+      message: 'invalid token',
+    });
+  }
 });
 
 module.exports = router;
