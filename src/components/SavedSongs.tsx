e5bde1c69abe3fdfa184050661f0d605ec011fc3
Haroon Khan <HaroonKhan120704@gmail.com>
2023-05-30 00:20:39 +0100

    added username on account

--- src/components/SavedSongs.tsx
+++ src/components/SavedSongs.tsx
@@ -1,9 +1,11 @@
 import Cookies from 'js-cookie';
-import React, { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { decodeToken } from 'react-jwt';
 
 export default function SavedSongs({ handleSongClick, songs }: any) {
+  const [clientUsername, setClientUsername] = useState('');
+
   const navigate = useNavigate();
   const backToHome = () => {
     Cookies.remove('UserjwtToken');
@@ -11,17 +13,23 @@ export default function SavedSongs({ handleSongClick, songs }: any) {
   };
   const usernameJWT = () => {
     const getJWT = Cookies.get('UserjwtToken');
-    const decodedToken = decodeToken(getJWT);
-    console.log(getJWT);
-    console.log(decodedToken);
+
+    if (getJWT) {
+      const decodedTokenUsername = (decodeToken(getJWT) as { username: string })
+        .username;
+      setClientUsername(decodedTokenUsername);
+    } else return;
   };
   useEffect(() => {
     usernameJWT();
   });
   return (
     <div className='border border-white flex flex-col bg-neutral-700 text-center p-2'>
-      <h2>Account: {}</h2>
-      <h2 onClick={backToHome} className='border border-white'>
+      <h2 className='font-bold text-lg'>Account: {clientUsername}</h2>
+      <h2
+        onClick={backToHome}
+        className='border-2 border-white rounded-2xl p-2 m-2 cursor-pointer font-bold'
+      >
         Log Out
       </h2>
       <h2 className='text-xl font-bold'>This is your Queue</h2>
@@ -29,7 +37,7 @@ export default function SavedSongs({ handleSongClick, songs }: any) {
         <p
           key={index}
           onClick={() => handleSongClick(song)}
-          className='cursor-pointer text-black bg-white font-bold'
+          className='cursor-pointer text-black bg-white font-bold border border-neutral-900 rounded-lg'
         >
           {song.name}
         </p>
