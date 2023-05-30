4bbe406e6c4ac9787277482593713853e379ac13
Haroon Khan <HaroonKhan120704@gmail.com>
2023-05-31 00:01:17 +0100

    created new route

--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,6 @@
 function routes(app, { urlencodedParser }) {
   app.use('/user', urlencodedParser, require('./user'));
   app.use('/auth', urlencodedParser, require('./auth'));
+  app.use('/songs', urlencodedParser, require('./songs'));
 }
 module.exports = routes;
