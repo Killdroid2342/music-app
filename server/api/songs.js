faa16b44b32d2dfbe14189f40e7b4b9a57d48dba
Haroon Khan <HaroonKhan120704@gmail.com>
2023-06-01 00:07:30 +0100

    uploaded songs

--- server/api/songs.js
+++ server/api/songs.js
@@ -2,9 +2,18 @@ const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
 require('dotenv').config();
+const { uploadSongs } = require('../modal/song');
+
+router.use(bodyParser.json());
 
 router.post('/user-songs', async (req, res) => {
+  const { username, date_added, songname, uuid } = req.body;
   console.log('you have hit the endpoint');
+  console.log(username);
+  console.log(date_added);
+  console.log(songname);
+  console.log(uuid);
+  uploadSongs(username, date_added, songname, uuid);
 });
 
 module.exports = router;
