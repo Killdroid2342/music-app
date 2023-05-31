1f12d30ceb973c49e49552dc2bc0098057e3c243
Haroon Khan <HaroonKhan120704@gmail.com>
2023-06-01 00:06:27 +0100

    inserting songs into database

--- server/modal/song.js
+++ server/modal/song.js
@@ -1,5 +1,19 @@
 const mysql = require('mysql2');
 
-const uploadSongs = () => {};
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'musicplayer',
+});
 
-module.exports = uploadSongs;
+const uploadSongs = async (username, date_added, songname, uuid) => {
+  conn.query(
+    'INSERT INTO songs (username, date_added, songname, UUID) VALUES (?,?,?,?)',
+    [username, date_added, songname, uuid]
+  );
+};
+
+module.exports = {
+  uploadSongs,
+};
