25ea0c8cc48e03a91e8e4634b23633d45a43f62f
Haroon Khan <HaroonKhan120704@gmail.com>
2023-07-14 21:59:13 +0100

    more auth

---
+++ src/hooks/Auth.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import Cookies from 'js-cookie';
+const { VITE_API_URL } = import.meta.env;
+
+function Auth() {
+  const [user, setUser] = useState('');
+  const Nav = useNavigate();
+  const checkToken = async () => {
+    const cookieVal = Cookies.get('UserjwtToken');
+    if (cookieVal !== undefined) {
+      const instance = axios.create({ baseURL: VITE_API_URL });
+      const res = await instance.post('/auth/validate-token', {
+        isAuth: cookieVal,
+      });
+      if (res.data.isAuth == false) {
+        console.log(res.data.isAuth);
+        Nav('/');
+      }
+      setUser(res.data.isAuth);
+    } else {
+      Nav('/');
+    }
+  };
+  useEffect(() => {
+    checkToken();
+  }, []);
+  return user;
+}
+export default Auth;
