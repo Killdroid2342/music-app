1db13b1635cdd30dc118fa43f8b73ea2e709f51c
Haroon Khan <HaroonKhan120704@gmail.com>
2023-11-04 19:02:43 +0000

    renamed table

--- server/modal/song.js
+++ server/modal/song.js
@@ -9,20 +9,22 @@ const conn = mysql.createConnection({
 
 const uploadSongs = async (username, date_added, songname, uuid) => {
   conn.query(
-    'INSERT INTO songs (username, date_added, songname, UUID) VALUES (?,?,?,?)',
+    'INSERT INTO musicplayer_songs (username, date_added, songname, UUID) VALUES (?,?,?,?)',
     [username, date_added, songname, uuid]
   );
 };
 const getSongs = async (username) => {
   const [rows, fields] = await conn
     .promise()
-    .query('SELECT * FROM songs WHERE username = ?', [username]);
+    .query('SELECT * FROM musicplayer_songs WHERE username = ?', [username]);
   return rows;
 };
 
 const deleteSong = async (uuid) => {
   try {
-    await conn.promise().query('DELETE FROM songs WHERE UUID = ?', [uuid]);
+    await conn
+      .promise()
+      .query('DELETE FROM musicplayer_songs WHERE UUID = ?', [uuid]);
   } catch (e) {
     console.log(e);
     throw e;
