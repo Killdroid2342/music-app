74ee8db8b824878481c76f8725d420e267c2295c
Haroon Khan <HaroonKhan120704@gmail.com>
2023-11-09 00:51:08 +0000

    added ID to file and added bucket files

--- server/api/songs.js
+++ server/api/songs.js
@@ -4,23 +4,21 @@ const bodyParser = require('body-parser');
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
-const AWS = require('aws-sdk');
 const multerS3 = require('multer-s3');
-const { s3 } = require('../util');
 require('dotenv').config();
 const { uploadSongs, getSongs, deleteSong } = require('../modal/song');
+const { s3 } = require('../util');
+require('aws-sdk/lib/maintenance_mode_message').suppress = true;
 
 router.use(bodyParser.json());
-
 const upload = multer({
   storage: multerS3({
     s3: s3,
     bucket: process.env.AWS_BUCKET_NAME,
     metadata: function (req, file, cb) {
       cb(null, { fieldName: file.fieldname });
-    },
-    UUID: function (req, file, cb) {
-      cb(null, Date.now().toString());
+      console.log(file.fieldname, 'THIS IS FIELD NAME');
+      console.log(file, 'THIS IS FILE');
     },
   }),
 });
@@ -28,32 +26,47 @@ const upload = multer({
 router.post('/upload-song', upload.single('files'), async (req, res) => {
   try {
     const { username, songname, UUID } = req.body;
-    const originalFileName = req.file.originalname; // Get the original filename
-    console.log(originalFileName); // Log the original file name
-    const dateOfSongAdded = new Date();
-    console.log(req.file);
+    console.log(UUID, 'make this the name in the S3 bucket');
 
-    uploadSongs(username, dateOfSongAdded, songname, UUID);
-    res.send({
-      message: 'You have successfully uploaded song :)',
-      UUID: UUID,
+    const dateOfSongAdded = new Date();
+
+    // Rename the uploaded file with the UUID as its name in the S3 bucket
+    const params = {
+      Bucket: process.env.AWS_BUCKET_NAME,
+      Key: UUID,
+      Body: req.file.buffer,
+    };
+
+    s3.putObject(params, function (err, data) {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error uploading to S3');
+      } else {
+        // Save the song information in the database
+        uploadSongs(username, dateOfSongAdded, songname, UUID);
+        res.send({
+          message: 'You have successfully uploaded song :)',
+          UUID: UUID,
+        });
+      }
     });
   } catch (err) {
-    console.log(err);
-    res.send({
-      message: 'There has been an error uploading your songs :(',
-    });
+    console.error(err);
+    res.status(500).send('There has been an error uploading your songs :(');
   }
 });
 
 // GET SONGS FROM S3 AND DATABASE (MAKE IT MATCH)
 
 router.post('/get-songs', async (req, res) => {
-  const { clientUsername } = req.body;
+  const { clientUsername, UUID } = req.body;
+  const { ID } = req.params;
+  console.log(ID, 'THIS IS ID');
+  console.log(UUID, 'This is UUID');
   const params = {
     Bucket: process.env.AWS_BUCKET_NAME,
   };
-  s3.listObjects(params, function (err, data) {
+  s3.listObjectsV2(params, function (err, data) {
     if (err) throw err;
     console.log(data);
   });
@@ -64,8 +77,12 @@ router.post('/get-songs', async (req, res) => {
 
 router.get('/song/:ID', (req, res) => {
   const { ID } = req.params;
-  const pathUrl = path.join(__dirname, process.env.AWS_BUCKET_NAME + ID);
-  console.log(__dirname);
+  console.log(ID, 'THIS IS ID');
+
+  // console.log(ID);
+  const pathUrl = path.join(__dirname, '../uploads/musicTMP/' + ID);
+  console.log(__dirname, 'this is dirname');
+  // console.log(__dirname);
   res.sendFile(pathUrl);
 });
 
