d89a59ce3885273d77013f03b9bc2d59676f2e69
Haroon Khan <HaroonKhan120704@gmail.com>
2023-11-09 00:50:14 +0000

    added bucket

--- server/util.js
+++ server/util.js
@@ -8,6 +8,7 @@ function getDbConn() {
 const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  bucket: process.env.AWS_BUCKET_NAME,
 });
 
 module.exports = {
