ed947dc982edc7ec20184eb5fa0b2eddd9d0a9fc
Haroon Khan <HaroonKhan120704@gmail.com>
2023-11-13 14:27:00 +0000

    fixing small errors

--- server/index.js
+++ server/index.js
@@ -10,8 +10,8 @@ const routes = require('./api/index');
 const corsOptions = {
   origin: [
     'http://localhost:5173',
-    'https://music-app-killdroid2342.vercel.app/',
-    'https://music-app-opal-one.vercel.app/',
+    'https://music-app-killdroid2342.vercel.app',
+    'https://music-app-opal-one.vercel.app',
   ],
   optionsSuccessStatus: 200,
   credentials: true,
