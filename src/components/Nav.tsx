004580629a742c0928f39e4a4686ac3a1880c835
Haroon Khan <HaroonKhan120704@gmail.com>
2023-12-22 10:40:44 +0000

    added jsx nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import AccountSettingsModal from './SavedSongs/AccountSettings/AccountSettingsModal';
 
-const Nav = () => {
-  return <div></div>;
+const Nav = ({ clientUsername }: any) => {
+  const [isAccountSettingsModalOpen, setAccountSettingsModalOpen] =
+    useState(false);
+  const openSettings = () => {
+    setAccountSettingsModalOpen(true);
+  };
+  return (
+    <div className='flex flex-col justify-center'>
+      <h2 className='font-bold text-lg text-center'>
+        Account: {clientUsername}
+      </h2>
+
+      <p
+        onClick={openSettings}
+        className='text-center border-2 border-white rounded-2xl p-2 m-2 cursor-pointer font-bold'
+      >
+        Account Settings
+      </p>
+      <p className='text-center border-2 border-white rounded-2xl p-2 m-2 cursor-pointer font-bold'>
+        Nav
+      </p>
+      {isAccountSettingsModalOpen && (
+        <AccountSettingsModal
+          setAccountSettingsModalOpen={setAccountSettingsModalOpen}
+          clientUsername={clientUsername}
+        />
+      )}
+    </div>
+  );
 };
 
 export default Nav;
