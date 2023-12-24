a111c31ad36cd9109a063d7f6004f6a42390a72c
Haroon Khan <HaroonKhan120704@gmail.com>
2023-12-24 13:03:43 +0000

    updated nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,31 +1,45 @@
 import { useState } from 'react';
 import AccountSettingsModal from './SavedSongs/AccountSettings/AccountSettingsModal';
 import { useNavigate } from 'react-router-dom';
+
 const Nav = ({ clientUsername }: any) => {
   const [isAccountSettingsModalOpen, setAccountSettingsModalOpen] =
     useState(false);
   const navigate = useNavigate();
+
   const openSettings = () => {
     setAccountSettingsModalOpen(true);
   };
-  return (
-    <div className='flex flex-col justify-center'>
-      <h2 className='font-bold text-lg text-center'>
-        Account: {clientUsername}
-      </h2>
 
-      <p
-        onClick={openSettings}
-        className='text-center border-2 border-white rounded-2xl p-2 m-2 cursor-pointer font-bold'
-      >
-        Account Settings
-      </p>
-      <p
-        onClick={() => navigate('/nav')}
-        className='text-center border-2 border-white rounded-2xl p-2 m-2 cursor-pointer font-bold'
-      >
-        Nav
-      </p>
+  return (
+    <div className='flex flex-col items-center bg-neutral-700 border border-white'>
+      <h2 className='font-bold text-lg my-2'>Account: {clientUsername}</h2>
+      <div className='flex flex-row'>
+        <p
+          onClick={openSettings}
+          className='text-center border-2 border-white rounded-2xl p-2 m-2 cursor-pointer font-bold'
+        >
+          Account Settings
+        </p>
+        <p
+          onClick={() => navigate('/social')}
+          className='text-center border-2 border-white rounded-2xl p-2 m-2 cursor-pointer font-bold'
+        >
+          Social
+        </p>
+        <p
+          onClick={() => navigate('/main')}
+          className='text-center border-2 border-white rounded-2xl p-2 m-2 cursor-pointer font-bold'
+        >
+          Music Page
+        </p>
+        <p
+          onClick={() => navigate('/friends')}
+          className='text-center border-2 border-white rounded-2xl p-2 m-2 cursor-pointer font-bold'
+        >
+          Friends
+        </p>
+      </div>
       {isAccountSettingsModalOpen && (
         <AccountSettingsModal
           setAccountSettingsModalOpen={setAccountSettingsModalOpen}
