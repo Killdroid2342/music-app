e09b73c30c2e1b98aac8f55857defc3df93637de
Haroon Khan <HaroonKhan120704@gmail.com>
2023-12-29 13:07:26 +0000

    added another axios request

--- src/Pages/Following.tsx
+++ src/Pages/Following.tsx
@@ -1,11 +1,24 @@
 import Nav from '../components/Nav';
 import { useState, useEffect } from 'react';
+const { VITE_API_URL } = import.meta.env;
 
 import Cookies from 'js-cookie';
 import { decodeToken } from 'react-jwt';
+import axios from 'axios';
 const Following = () => {
+  const instance = axios.create({
+    baseURL: VITE_API_URL,
+  });
   const [clientUsername, setClientUsername] = useState('');
+  const [followingAmount, setFollowingAmount] = useState();
   console.log(clientUsername);
+  const getFollowingAmount = async () => {
+    const res = await instance.post('/user/getfollowercount', {
+      username: clientUsername,
+    });
+    setFollowingAmount(res.data);
+  };
+  console.log(followingAmount);
   const usernameJWT = () => {
     const getJWT = Cookies.get('UserjwtToken');
     if (getJWT) {
@@ -16,13 +29,16 @@ const Following = () => {
   };
   useEffect(() => {
     usernameJWT();
+    // getFollowingAmount();
   });
+  useEffect(() => {}, []);
   return (
     <>
       <Nav clientUsername={clientUsername} />
       <h1 className='text-center text-3xl mt-5 mb-5 font-bold'>
-        Here are the people you added
+        Here are the people you follow
       </h1>
+      {/* <p>{followingAmount[0].followers}</p> */}
     </>
   );
 };
