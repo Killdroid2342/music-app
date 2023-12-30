e852428c659edd334c8c560286ff6fe0c8c3042d
Haroon Khan <HaroonKhan120704@gmail.com>
2023-12-30 00:30:48 +0000

    fixed getting amount

--- src/Pages/Following.tsx
+++ src/Pages/Following.tsx
@@ -5,40 +5,58 @@ const { VITE_API_URL } = import.meta.env;
 import Cookies from 'js-cookie';
 import { decodeToken } from 'react-jwt';
 import axios from 'axios';
+
 const Following = () => {
   const instance = axios.create({
     baseURL: VITE_API_URL,
   });
+
   const [clientUsername, setClientUsername] = useState('');
-  const [followingAmount, setFollowingAmount] = useState();
+  const [followingAmount, setFollowingAmount] = useState<any>(null);
   console.log(clientUsername);
+
   const getFollowingAmount = async () => {
-    const res = await instance.post('/user/getfollowercount', {
-      username: clientUsername,
-    });
-    setFollowingAmount(res.data);
+    try {
+      const res = await instance.post('/user/getfollowercount', {
+        username: clientUsername,
+      });
+      setFollowingAmount(res.data);
+    } catch (error) {
+      console.error('Error fetching following amount:', error);
+    }
   };
-  console.log(followingAmount);
+
   const usernameJWT = () => {
     const getJWT = Cookies.get('UserjwtToken');
     if (getJWT) {
       const decodedTokenUsername = (decodeToken(getJWT) as { username: string })
         .username;
       setClientUsername(decodedTokenUsername);
-    } else return;
+    } else {
+      console.log('error getting JWT');
+    }
   };
+
   useEffect(() => {
     usernameJWT();
-    // getFollowingAmount();
-  });
-  useEffect(() => {}, []);
+    getFollowingAmount();
+  }, [clientUsername]);
+
   return (
     <>
       <Nav clientUsername={clientUsername} />
       <h1 className='text-center text-3xl mt-5 mb-5 font-bold'>
         Here are the people you follow
       </h1>
-      {/* <p>{followingAmount[0].followers}</p> */}
+      {followingAmount === null ? (
+        <p>Loading...</p>
+      ) : followingAmount.length === 0 ? (
+        <p>No followers found.</p>
+      ) : (
+        <p>Amount of followers: {followingAmount[0]?.followers}</p>
+      )}
+      <p>Accounts you follow</p>
+      {/* USE .MAP TO MAP OUT THE ACCOUNTS YOU FOLLOW IN A 3 by 3 */}
     </>
   );
 };
