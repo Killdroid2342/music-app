d24fa02dcf72107b367b8fa0866efa0c1ef67934
Haroon Khan <HaroonKhan120704@gmail.com>
2024-01-07 18:55:22 +0000

    deleted jsx

--- src/Pages/Following.tsx
+++ src/Pages/Following.tsx
@@ -12,13 +12,9 @@ const Following = () => {
   });
 
   const [clientUsername, setClientUsername] = useState('');
-  const [lengthOfFollowers, setLengthOfFollowers] = useState(0);
-  console.log(lengthOfFollowers, 'this is length');
-  const [userFollowing, setUserFollowing] = useState([{}]);
-  console.log(userFollowing);
-  console.log(userFollowing, 'THIS IS USER FOLLOWING USEsTATE');
-  console.log(clientUsername, 'this is clientusername');
-
+  // const [lengthOfFollowers, setLengthOfFollowers] = useState(0);
+  const [following, setFollowing] = useState('');
+  console.log(following);
   const usernameJWT = () => {
     const getJWT = Cookies.get('UserjwtToken');
     if (getJWT) {
@@ -29,28 +25,26 @@ const Following = () => {
       console.log('error getting JWT');
     }
   };
-  useEffect(() => {
-    usernameJWT();
-  }, []);
 
-  useEffect(() => {
-    console.log('Client Username:', clientUsername);
-    if (clientUsername) {
-      getUserFollowings();
-    }
-  }, [clientUsername]);
-
-  const getUserFollowings = async () => {
+  const FollowingUsers = async () => {
     try {
-      const res = await instance.get('/user/getFollowingUsers', {
-        params: { username: clientUsername },
-      });
-      setUserFollowing(res.data);
-      setLengthOfFollowers(userFollowing.length);
-    } catch (e) {
-      console.error('error:', e);
+      const res = await instance.get('/user/following-users');
+      console.log(res);
+      console.log(res.data);
+      setFollowing(res.data);
+    } catch (error) {
+      console.error('Error fetching following users', error);
     }
   };
+
+  useEffect(() => {
+    usernameJWT();
+  });
+  useEffect(() => {
+    if (clientUsername) {
+      FollowingUsers();
+    }
+  }, []);
   return (
     <>
       <Nav clientUsername={clientUsername} />
@@ -58,16 +52,16 @@ const Following = () => {
         Here are the people you follow
       </h1>
       <p className='text-center text-2xl'>
-        Accounts you follow: {lengthOfFollowers}
+        {/* Accounts you follow: {lengthOfFollowers} */}
       </p>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-8'>
-        {userFollowing.map((userFollowing: any, key: any) => (
+        {/* {userFollowing.map((userFollowing: any, key: any) => (
           <div key={key} className='bg-gray-200 p-4 rounded-md'>
             <p className='text-center text-lg font-semibold text-neutral-900 p-4'>
               {userFollowing.username}
             </p>
           </div>
-        ))}
+        ))} */}
       </div>
     </>
   );
